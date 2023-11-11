0bf3f0a3c7f9e3bc4ed6e351b4ee505ce72a0c13
orkhanvib <orkhanvib@TITAN08>
2023-11-11 18:57:43 +0400

    Add project files.

---
+++ ConsoleApp132/Program.cs
@@ -0,0 +1,7 @@
+﻿using ConsoleApp132.NewFolder;
+
+Cars car1 = new();
+car1.HorsePower = 955;
+Console.WriteLine(car1.HorsePower);
+Console.WriteLine(car1.Year);
+Console.WriteLine(car1.ShowName());
---
+++ ConsoleApp132/model/Cars.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp132.NewFolder
+{
+    internal class Cars
+    {
+        private int _horsepower;
+        public int HorsePower
+        {
+            get
+            {
+                return _horsepower;
+            }
+            set
+            {
+                if (value < 300)
+                {
+                    Console.WriteLine("BU MASININ AT GUCU 300DEN KICIK OLA BILMEZ");
+                    return;
+                }
+                _horsepower = value;
+            }
+        }
+        public readonly int Year=1999;
+
+        protected string Name;
+        public string ShowName()
+        {
+            return Name;
+        }
+    }
+}
